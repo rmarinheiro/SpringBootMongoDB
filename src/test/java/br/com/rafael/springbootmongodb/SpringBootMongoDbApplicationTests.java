c3c1694a472c3391365642e7757bccaf48fe79c5
=Rafael Marinheiro <=rsmarinheiro007@gmail.com>
2020-06-27 21:51:27 -0300

    Primeiro Commit

---
+++ src/test/java/br/com/rafael/springbootmongodb/SpringBootMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafael.springbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
