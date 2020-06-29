040cf6840deff0af2ff95072ba67a4afa07a8407
=Rafael Marinheiro <=rsmarinheiro007@gmail.com>
2020-06-29 00:25:40 -0300

    Criando o padrao DTO

--- src/main/java/br/com/rafael/springbootmongodb/resource/UserResource.java
+++ src/main/java/br/com/rafael/springbootmongodb/resource/UserResource.java
@@ -3,6 +3,8 @@ package br.com.rafael.springbootmongodb.resource;
 
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rafael.springbootmongodb.domain.User;
+import br.com.rafael.springbootmongodb.dto.UserDTO;
 import br.com.rafael.springbootmongodb.services.UserService;
 
 @RestController
@@ -21,10 +24,11 @@ public class UserResource {
 	private UserService userService;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		
 		List<User>list = userService.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 		
 		
 	}
